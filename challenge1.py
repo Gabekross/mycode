d50329f7b5b329ba6e0637100e9552ab896d0ec7
Gabekross <Gabekross@users.noreply.github.com>
2022-09-19 22:29:51 +0000

    laermin scripting

---
+++ challenge1.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+
+def main():
+
+    user_name = input("Enter your name:")
+    week_day = input("what day of the is it? e.g Monday:")
+    print("Hello, " + user_name + "!"+ " Happy " + week_day +"!")
+
+main()
+
