e6a1c53fe7ee63a42a9b11b53917fdb830ea0ed2
Gabekross <Gabekross@users.noreply.github.com>
2022-09-22 16:42:01 +0000

    looping techniques

---
+++ fact/looping02.py
@@ -0,0 +1,15 @@
+#!enecccbnienhntretctldghkuvuviutfvurjnlghvbig
+#!/usr/bin/env python3
+
+#open file in read mode
+
+dnsfile =open("dnsservers.txt", "r")
+
+dnslist = dnsfile.readlines()
+
+for svr in dnslist:
+    print(svr, end="")
+
+dnsfile.close()
+
+
